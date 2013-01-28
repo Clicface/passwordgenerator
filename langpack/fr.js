a5730a80415415287c7a7c53450b5cc59c3e8c94
Clicface <github@clicface.fr>
2013-01-28 23:43:30 +0100

    Initial import

---
+++ langpack/fr.js
@@ -0,0 +1,10 @@
+jquery_lang_js.prototype.lang.fr = {
+	'password character length': 'nombre de caract&egrave;res dans le mot de passe',
+	'string letters': 'lettres minuscules',
+	'capital letters': 'lettres majuscules',
+	'digits': 'chiffres',
+	'special symbols': 'caract&egrave;res sp&eacute;ciaux',
+	'ambiguous characters': 'caract&egrave;res ambigus',
+	'Generate Password': 'G&eacute;n&eacute;rer le mot de passe',
+	'Please make at least one selection': 'veuillez effectuer au moins un choix',
+}
